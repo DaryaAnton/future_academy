4ea8dbe704c4d80b9aecccc104b606d5225b694d
Kseniya Yaskevich <yaskevi4kseniya@gmail.com>
2024-10-11 12:18:10 +0300

    feat: добавление переключения выпадающего списка

--- src/assets/scripts/modules/menu.js
+++ src/assets/scripts/modules/menu.js
@@ -3,9 +3,17 @@ export const menu = () => {
   const button = body.querySelector('.navbar-toggle');
   const navbar = body.querySelector('.navbar-collapse');
 
+  const dropdown = body.querySelector('#mobile-dropdown');
+  const dropdownMenu = dropdown.querySelector('.dropdown-menu');
+
   button.addEventListener('click', () => {
     body.classList.toggle('page-body--lock');
     button.classList.toggle('active');
     navbar.classList.toggle('show');
+  });
+
+  dropdown.addEventListener('click', () => {
+    dropdown.classList.toggle('active');
+    dropdownMenu.classList.toggle('show');
   });
 };
