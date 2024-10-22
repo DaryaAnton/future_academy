49ad4031f899885c3a884fa662a3468f3d176055
DaryaA <12345@nail.com>
2024-10-22 09:12:39 +0500

    fix: изменения путей подключения файла cardSlider

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,5 +1,3 @@
 import { menu } from './base/menu.js';
-import { cardSlider } from './modules/cardSlider.js';
 
 menu(); //подключается по всему сайту
-cardSlider(); //подключать на нужной странице
--- src/index/scripts/script.js
+++ src/index/scripts/script.js
@@ -1,4 +1,6 @@
 import '@/assets/scripts/script';
 import { video } from '@/assets/scripts/modules/video.js';
+import { cardSlider } from '@/assets/scripts/modules/cardSlider.js';
 
 video();
+cardSlider();
