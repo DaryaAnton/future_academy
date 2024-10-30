3a2e6d42bbd38e1761dee3e98ed63d6d8a4da169
DaryaA <12345@nail.com>
2024-10-30 17:30:53 +0500

    fix: подключение js бутстрапа для табов

--- src/index/scripts/script.js
+++ src/index/scripts/script.js
@@ -1,4 +1,5 @@
 import '@/assets/scripts/script';
+import 'bootstrap/js/src/tab'
 import { video } from '@/assets/scripts/modules/video.js';
 import { cardSlider } from '@/assets/scripts/modules/cardSlider.js';
 
