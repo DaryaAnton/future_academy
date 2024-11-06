4cae59835630086f0165431447833c5b2baa06cc
DaryaA <12345@nail.com>
2024-11-06 18:14:00 +0500

    fix: подключение js модального окна к index.html

--- src/index/scripts/script.js
+++ src/index/scripts/script.js
@@ -1,5 +1,6 @@
 import '@/assets/scripts/script';
 import 'bootstrap/js/src/tab'
+import 'bootstrap/js/src/modal'
 import { video } from '@/assets/scripts/modules/video.js';
 import { cardSlider } from '@/assets/scripts/modules/cardSlider.js';
 
