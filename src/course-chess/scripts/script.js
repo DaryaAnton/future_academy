114290300160eda17241d77a15713a82e8d99411
DaryaA <12345@nail.com>
2024-11-18 15:18:03 +0500

    fix: добавление путей js для стр course-chess

--- src/course-chess/scripts/script.js
+++ src/course-chess/scripts/script.js
@@ -1,3 +1,10 @@
 import '@/assets/scripts/script';
 import 'bootstrap/js/src/modal'
+import 'bootstrap/js/dist/collapse'
+
+import { video } from '@/assets/scripts/modules/video.js';
+import { cardSlider } from '@/assets/scripts/modules/cardSlider.js';
+
+video();
+cardSlider();
 
