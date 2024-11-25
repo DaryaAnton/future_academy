c8c4b0c973dd355fa604d5ffe001f9a80003352c
DaryaA <12345@nail.com>
2024-11-25 17:00:55 +0500

    feat: добавление getData для event.js

--- src/events/scripts/script.js
+++ src/events/scripts/script.js
@@ -1,3 +1,9 @@
 import '@/assets/scripts/script';
 import 'bootstrap/js/src/modal'
 
+import { getData } from '../../assets/scripts/modules/getData';
+
+getData('/events.json').then((data) => {
+  console.log(data);
+});
+
