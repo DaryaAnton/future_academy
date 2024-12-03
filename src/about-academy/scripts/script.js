af8ae522cd9c5d5d18d04676bd0c7c33626e81ff
DaryaA <12345@nail.com>
2024-12-03 13:51:26 +0500

    feat: добавлен script.js и импорты для about-academy.html

---
+++ src/about-academy/scripts/script.js
@@ -0,0 +1,9 @@
+import '@/assets/scripts/script';
+import 'bootstrap/js/src/modal'
+
+import { video } from '@/assets/scripts/modules/video.js';
+import { cardSlider } from '@/assets/scripts/modules/cardSlider.js';
+
+cardSlider();
+video();
+
